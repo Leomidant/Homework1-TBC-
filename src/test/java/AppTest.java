091f835d97532fcb3d99829e866a8d1bbc61b789
Leomidant <cartoonx65@gmail.com>
2020-12-02 21:06:22 +0400

    Second commit

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,13 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static io.github.bonigarcia.wdm.config.DriverManagerType.CHROME;
+
+//xpath("/html/body/div[2]/div/ul/li[20]/a"))
 public class AppTest {
     public static void main(String[] args){
+        WebDriverManager.getInstance(CHROME).setup();
         WebDriver driver = new ChromeDriver();
         driver.get("http://the-internet.herokuapp.com/");
-        driver.findElement(By.xpath("/html/body/div[2]/div/ul/li[20]/a")).click();
-        driver.findElement(By.id("email")).sendKeys("cartoonx65@gmail.com");
-        driver.findElement(By.xpath("/html/body/div[2]/div/div/form/button/i")).click();
+        driver.findElement(By.xpath("//*[@id=\"content\"]/ul/li[20]/a")).click();
+        driver.findElement(By.id("email")).sendKeys("test@gmail.com");
+        driver.findElement(By.id("form_submit")).click();
     }
 }
